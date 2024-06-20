30d7bf9ef8b8fee166e3c958adee41f0e82a2809
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-06-20 21:00:14 +0300

    feat: :lipstick: update Card component in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,23 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useState } from "react";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [totalSpent, setTotalSpent] = useState(0);
   return (
     <>
-      <div>
-        <button onClick={() => setCount(count + 1)}>Up</button>
-        <button onClick={() => setCount(count - 1)}>Down</button>
-      </div>
-      <p>{count}</p>
+      <Card className="w-[350px] mx-auto ">
+        <CardHeader>
+          <CardTitle>Total Spent</CardTitle>
+          <CardDescription>The total amount you've spent</CardDescription>
+        </CardHeader>
+        <CardContent>{totalSpent}</CardContent>
+      </Card>
     </>
   );
 }
