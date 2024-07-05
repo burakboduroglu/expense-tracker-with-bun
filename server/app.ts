9fac51562bd4f62aa8fd64b05ba81886c718f3ae
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-07-05 21:07:02 +0300

    React Query init and process

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,12 @@ import {Hono} from "hono";
 import {logger} from "hono/logger";
 import {expensesRoute} from "./routes/expenses";
 import {serveStatic} from "hono/bun";
+import { cors } from 'hono/cors'
 
 const app = new Hono();
 
 app.use("*", logger());
+app.use('http://localhost:5173/', cors())
 
 const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute)
 
--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -33,7 +33,7 @@ export const expensesRoute = new Hono()
         fakeExpenses.push({...expense, id: fakeExpenses.length + 1});
         return c.json(expense);
     })
-    .get("/total-spent", (c) => {
+    .get("/total-spent", async (c) => {
         const total = fakeExpenses.reduce((acc, expense) => acc + expense.amount, 0)
         return c.json({total})
     })
