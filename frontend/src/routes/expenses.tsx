7dc51da49cb70c41db92923ad522eca6d2822b67
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-07-06 08:17:35 +0300

    Table and expense skeleton designed

--- frontend/src/routes/expenses.tsx
+++ frontend/src/routes/expenses.tsx
@@ -1,9 +1,73 @@
-import {createFileRoute} from '@tanstack/react-router'
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/components/ui/skeleton";
+import { createFileRoute } from "@tanstack/react-router";
+import { api } from "../../lib/api";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-export const Route = createFileRoute('/expenses')({
-    component: Expenses,
-})
+export const Route = createFileRoute("/expenses")({
+  component: Expenses,
+});
+async function getAllExpenses() {
+  const res = await api.expenses.$get();
+  if (!res.ok) throw new Error("server error");
+  const data = await res.json();
+  return data;
+}
 
 function Expenses() {
-    return <div className="p-2">Hello from expenses!</div>
+  const { isPending, data, error } = useQuery({
+    queryKey: ["get-all-expenses"],
+    queryFn: getAllExpenses,
+  });
+
+  if (error) return "An error has occurred:" + error;
+
+  return (
+    <div className="p-2 max-w-2xl mx-auto">
+      <Table>
+        <TableCaption>A list of your expenses.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Title</TableHead>
+            <TableHead>Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {isPending ? (
+            <ExpenseSkeleton/>
+          ) : (
+            data?.expenses.map((expense) => (
+              <TableRow key={expense.id}>
+                <TableCell className="font-medium">{expense.title}</TableCell>
+                <TableCell>{expense.amount}</TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+export function ExpenseSkeleton() {
+  return Array(3)
+    .fill(0)
+    .map((_, i) => {
+     return <TableRow key={i}>
+        <TableCell className="font-medium">
+          <Skeleton className="h-4 w-[250px]" />
+        </TableCell>
+        <TableCell>
+          <Skeleton className="h-4 w-[200px]" />
+        </TableCell>
+      </TableRow>;
+    });
 }
