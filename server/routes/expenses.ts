2f2f68ed548d911497145b4686b5959c524238fa
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2024-07-09 22:33:51 +0300

    Validation update

--- server/routes/expenses.ts
+++ server/routes/expenses.ts
@@ -2,7 +2,10 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { getUser } from "../kinde";
 import { db } from "../db";
-import { expenses as expenseTable } from "../db/schema/expenses";
+import {
+  expenses as expenseTable,
+  insertExpensesSchema,
+} from "../db/schema/expenses";
 import { and, desc, eq, sum } from "drizzle-orm";
 import { createExpenseSchema } from "../sharedTypes";
 
@@ -24,13 +27,16 @@ export const expensesRoute = new Hono()
     const expense = c.req.valid("json");
     const user = c.var.user;
 
+    const validatedExpense = insertExpensesSchema.parse({
+      ...expense,
+      userId: user.id,
+    });
+
     const result = await db
       .insert(expenseTable)
-      .values({
-        ...expense,
-        userId: user.id,
-      })
-      .returning();
+      .values(validatedExpense)
+      .returning()
+      .then((res) => res[0]);
 
     c.status(201);
     return c.json(result);
--- server/sharedTypes.ts
+++ server/sharedTypes.ts
@@ -1,9 +1,8 @@
 import { z } from "zod";
+import { insertExpensesSchema } from "./db/schema/expenses";
 
-export const expenseSchema = z.object({
-  id: z.number().int().positive().min(1),
-  amount: z.string().regex(/^\d+(\.\d{1,2})?$/, "Invalid amount"),
-  title: z.string().min(3, "Title must be at least 3 characters"),
+export const createExpenseSchema = insertExpensesSchema.omit({
+  userId: true,
+  createdAt: true,
+  id: true,
 });
-
-export const createExpenseSchema = expenseSchema.omit({ id: true });
